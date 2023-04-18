66096977230b7b707ce9a50e898bfe8bbc883cda
kdj <es-kbsys24@hybecorp.com>
2023-04-19 03:13:31 +0900

    로그인 페이지 퍼블리싱 시작

---
+++ src/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="login-wrap">
+  <div class="img-area"></div>
+  <div class="login-area">
+    <form action="">
+      <ul class="login-sub">
+
+
+
+
+        
+      </ul>
+    </form>
+  </div>
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
